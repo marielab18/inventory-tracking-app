435405540a1e28a8d65371ce244a2fe2e569cb6e
Mariela Belaunde <mariela.belaunde@gmail.com>
2022-06-12 21:02:16 -0400

    Add controller actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,44 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update, :destroy]
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+
+    redirect_ to items_path
+  end
+
+  def edit
+  end
+
+  def update
+    @item.update(item_params)
+
+    redirect_ to items_path
+  end
+
+  def destroy
+    @item.destroy
+
+    redirect_to items_path
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  validates :name, uniqueness: true
+  validates :name, :description, :price, presence: true
+
+
 end
